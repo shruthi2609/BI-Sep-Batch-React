38682ec0aa8c712a2de0b809ad9e4c58567f26d2
shruthi093 <jayasruthi.l@gmail.com>
2023-03-26 12:48:33 +0530

    v1.0

--- server/models/ContactManager.js
+++ server/models/ContactManager.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose")
 /*mongoose.connect("mongodb://localhost:27017/BIDBSEP").then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))*/
-mongoose.connect("mongodb+srv://shruthi:test123@cluster0.rpq7s.mongodb.net/?retryWrites=true&w=majority").then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))
+mongoose.connect(MONGO_ATLAS_URL).then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))
 
 const ContactManager=mongoose.model("ContactManager",{
    fname:{
--- server/models/userDetails.js
+++ server/models/userDetails.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose")
-mongoose.connect("mongodb+srv://shruthi:test123@cluster0.rpq7s.mongodb.net/?retryWrites=true&w=majority").then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))
+mongoose.connect(MONGO_ATLAS_URL).then((res)=>console.log("connected to db successfully")).catch((err)=>console.log("connection failed",err))
 
 
 const userDetails=mongoose.model("userDetails",{
